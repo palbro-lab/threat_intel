332b8aaa3e152376480e72637d7f9f201f209354
palbro-lab <palh99527@gmail.com>
2025-04-23 20:18:10 +0530

    Update config.py
    galti se key daldi thi

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-OTX_API_KEY = "c7b7d98840df9a4d36c836f54f66da350b5c0da918ed16e46130a5b837eceee9"  # Your OTX API key from https://otx.alienvault.com/
+OTX_API_KEY = "otx api key"  # Your OTX API key from https://otx.alienvault.com/
 MISP_FEED_URL = "https://www.circl.lu/doc/misp/feed-osint/"  # CIRCL public OSINT feed
 ORGANIZATION_ASSETS = {
     "ip_ranges": ["103.77.241.159"],
